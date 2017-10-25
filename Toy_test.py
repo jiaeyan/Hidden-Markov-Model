472dbbe898979264f447478ac1b22c08d06e5a2c
Jiajie Yan <svenyan234@gmail.com>
2017-10-25 12:01:00 -0400

    Update Toy_test.py

--- Toy_test.py
+++ Toy_test.py
@@ -15,6 +15,6 @@ hmm = HMM()
 hmm.train(train)
 hmm.test(test)
 print(hmm.T[:, 10].sum() == 1) # test if all transitions from one state add to 1 in Transition matrix.
-print(hmm.E[:, 10].sum() == 1) # test if all transitions from one state add to 1 in Emission maxtrix.
+print(hmm.E[:, 10].sum() == 1) # test if all emissions from one state add to 1 in Emission maxtrix.
 print(hmm.Pi.sum() == 1)       # test if all transitions from START state add to 1 in Pi maxtrix.
 print(hmm.forward(data[10]) == hmm.backward(data[10])) # test if the likelihood of Forward algorithm equals to Backforward.
